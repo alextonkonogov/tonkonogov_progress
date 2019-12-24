991ac989d73458e9de07e92b27485e170adcaa30
atonkonogov <tonkonogov.a@wildberries.ru>
2019-12-24 09:38:58 +0300

    - facade with testing and comments

--- facade/facade.go
+++ facade/facade.go
@@ -1,7 +1,11 @@
 package facade
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
+//NewComputerFacade creates computerFacade
 func NewComputerFacade() *computerFacade {
 	return &computerFacade{
 		cpu:       &cpu{},
@@ -10,42 +14,55 @@ func NewComputerFacade() *computerFacade {
 	}
 }
 
+// computerFacade implements facade
 type computerFacade struct {
 	cpu       *cpu
 	memory    *memory
 	hardDrive *hardDrive
 }
 
-func (cf *computerFacade) Start() {
+//Start returns how computer will start.
+func (f *computerFacade) Start() string {
 	position := "0x00"
-	cf.cpu.Freeze()
-	cf.memory.Load(position, cf.hardDrive.read(100, 1024))
-	cf.cpu.Jump(position)
-	cf.cpu.Execute()
+	result := []string{
+		f.cpu.Freeze(),
+		f.memory.Load(position, f.hardDrive.Read(100, 1024)),
+		f.cpu.Jump(position),
+		f.cpu.Execute(),
+	}
+	return strings.Join(result, "\n")
 }
 
+// cpu implements a subsystem "cpu"
 type cpu struct{}
 
-func (c *cpu) Freeze() {
-	fmt.Println("Freezing processor.")
+// Freeze implementation
+func (c *cpu) Freeze() string {
+	return fmt.Sprint("Freezing processor.")
 }
 
-func (c *cpu) Jump(position string) {
-	fmt.Printf("Jumping to: %v.\n", position)
+// Jump implementation
+func (c *cpu) Jump(position string) string {
+	return fmt.Sprintf("Jumping to: %v.", position)
 }
 
-func (c *cpu) Execute() {
-	fmt.Println("Executing.")
+// Execute implementation
+func (c *cpu) Execute() string {
+	return fmt.Sprint("Executing.")
 }
 
+// memory implements a subsystem "memory"
 type memory struct{}
 
-func (m *memory) Load(position, data string) {
-	fmt.Printf("Loading from %v data: '%v'.\n", position, data)
+// Load implementation
+func (m *memory) Load(position, data string) string {
+	return fmt.Sprintf("Loading from %v data: '%v'.", position, data)
 }
 
+// hardDrive implements a subsystem "hardDrive"
 type hardDrive struct{}
 
-func (h *hardDrive) read(lba, size int) string {
+// Read implementation
+func (h *hardDrive) Read(lba, size int) string {
 	return fmt.Sprintf("Some data from sector %d with size %d", lba, size)
 }
