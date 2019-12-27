3705ed82de697eec9d857ea63c49145c507abbdf
atonkonogov <tonkonogov.a@wildberries.ru>
2019-12-27 08:40:32 +0300

    - переделан тест согласно указанию

--- pkg/facade/computerFacade_test.go
+++ pkg/facade/computerFacade_test.go
@@ -3,18 +3,33 @@ package facade
 import (
 	"fmt"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
-var position, lba, size = "0x00", 100, 1024
+var tests = []struct {
+	position string
+	lba      int
+	size     int
+}{
+	{"0x00", 62, 128},
+	{"0x2a", 63, 256},
+	{"0b10", 945, 512},
+	{"0x20", 1007, 1024},
+	{"0x3e", 1070, 2048},
+	{"0x5b", 2015, 4096},
+}
 
 func TestFacade(t *testing.T) {
-	expect :=
-		fmt.Sprintf("Freezing processor.\n...sounds of working cpu...\nLoading from %v data: 'Some data from sector %d with size %d'.\n...sounds of working memory...\n...sounds of working hard drive...\nJumping to: %v.\nExecuting.",
-			position, lba, size, position)
-	computer := NewComputerFacade(position, lba, size)
-	result := computer.Start()
+	for i, tt := range tests {
+		t.Run(tt.position, func(t *testing.T) {
+			position, lba, size := tt.position, tt.lba, tt.size
+			expect := fmt.Sprintf("...sounds of starting computer...\nFreezing processor.\n...sounds of working cpu...\nLoading from %v data: 'Some data from sector %d with size %d'.\n...sounds of working memory...\n...sounds of working hard drive...\nJumping to: %v.\nExecuting.", position, lba, size, position)
+			computer := NewComputerFacade(position, lba, size)
+			result := computer.Start()
 
-	if result != expect {
-		t.Errorf("Expect result to equal %s, but %s.\n", expect, result)
+			assert.EqualValues(t, expect, result, "Result is not equal to the expected one")
+			t.Log("test №", i)
+		})
 	}
 }
